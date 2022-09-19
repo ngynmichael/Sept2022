828dba436738fb9fd872849679049bc9aee1e4a6
Michael Nguyen <michaeln@hcl.com>
2022-09-19 10:16:02 -0500

    Assignment 2

--- Sept2022/michael-app/src/main/java/com/michael/michael_app/MyClass.java
+++ Sept2022/michael-app/src/main/java/com/michael/michael_app/MyClass.java
@@ -1,5 +1,18 @@
 package com.michael.michael_app;
 
-public class MyClass {
+class Multiplication{
+	//Overload
+	void multiply(int x, int y) {
+		System.out.println(x * y);
+	}
+	void mulitply(double x, double y) {
+		System.out.println(x * y);
+	}
+}
 
+public class MyClass extends Multiplication{
+	// Override
+	void multiply(int x, int y, int z) {
+		System.out.println(x *  y * z);
+	}
 }
